e6dad9de16f7d932c67dcfee646c6dd3ec9cd3bb
Hackbright Student <info@hackbrightacademy.com>
2015-04-29 18:08:35 -0700

    Finished 4.1 of Shopping Site and working on 4.2

---
+++ shoppingsite.py
@@ -0,0 +1,134 @@
+"""Ubermelon shopping application Flask server.
+
+Provides web interface for browsing melons, seeing detail about a melon, and
+put melons in a shopping cart.
+
+Authors: Joel Burton, Christian Fernandez, Meggie Mahnken.
+"""
+
+
+from flask import Flask, render_template, redirect, flash, session
+import jinja2
+
+import model
+
+
+app = Flask(__name__)
+
+app.secret_key = 'Nuvi-Sadie'
+
+
+
+# Normally, if you refer to an undefined variable in a Jinja template,
+# Jinja silently ignores this. This makes debugging difficult, so we'll
+# set an attribute of the Jinja environment that says to make this an
+# error.
+
+app.jinja_env.undefined = jinja2.StrictUndefined
+
+
+@app.route("/")
+def index():
+    """Return homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route("/melons")
+def list_melons():
+    """Return page showing all the melons ubermelon has to offer"""
+
+    melons = model.Melon.get_all()
+    return render_template("all_melons.html",
+                           melon_list=melons)
+
+
+@app.route("/melon/<int:id>")
+def show_melon(id):
+    """Return page showing the details of a given melon.
+
+    Show all info about a melon. Also, provide a button to buy that melon.
+    """
+
+    melon = model.Melon.get_by_id(id)
+    print melon
+    return render_template("melon_details.html",
+                           display_melon=melon)
+
+
+@app.route("/cart")
+def shopping_cart():
+    """Display content of shopping cart."""
+
+    melon_list = session['cart']
+    melon_count_dict = {}
+    melon_qty_dict = {}
+    
+    
+    for melon in melon_list:
+        if melon in melon_count_dict:
+            melon_count_dict[melon] += 1
+        else:
+            melon_count_dict[melon] = 1
+
+    for melon_id in set(melon_list):
+        melon = model.get_by_id(melon_id)
+        melon_qty_dict[melon] = melon_count_dict[melon.id]
+
+
+
+    # TODO: Display the contents of the shopping cart.
+    #   - The cart is a list in session containing melons added
+
+    return render_template("cart.html", session=session)
+
+
+@app.route("/add_to_cart/<int:id>")
+def add_to_cart(id):
+    """Add a melon to cart and redirect to shopping cart page.
+
+    When a melon is added to the cart, redirect browser to the shopping cart
+    page and display a confirmation message: 'Successfully added to cart'.
+    """
+
+    # TODO: Finish shopping cart functionality
+    #   - use session variables to hold cart list
+    session.setdefault('cart', []).append(id)
+    flash("Melon added to cart.")
+
+    return redirect("/cart")
+
+
+@app.route("/login", methods=["GET"])
+def show_login():
+    """Show login form."""
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Log user into site.
+
+    Find the user's login credentials located in the 'request.form'
+    dictionary, look up the user, and store them in the session.
+    """
+
+    # TODO: Need to implement this!
+
+    return "Oops! This needs to be implemented"
+
+
+@app.route("/checkout")
+def checkout():
+    """Checkout customer, process payment, and ship melons."""
+
+    # For now, we'll just provide a warning. Completing this is beyond the
+    # scope of this exercise.
+
+    flash("Sorry! Checkout will be implemented in a future version.")
+    return redirect("/melons")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
